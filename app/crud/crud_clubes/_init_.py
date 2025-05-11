289ed862a67221ee611a706595071d509ece4c5a
Danna Prada <danna2211334@correo.uis.edu.co>
2025-05-10 20:37:11 -0500

    Guardando cambios antes de cambiar de rama

---
+++ app/crud/crud_clubes/_init_.py
@@ -0,0 +1,5 @@
+from .club_crear import crear_club
+from .club_listar import listar_clubes
+from .club_listar import obtener_club_por_id
+from .club_actualizar import actualizar_club
+from .club_eliminar import eliminar_club
